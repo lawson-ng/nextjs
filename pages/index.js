2424429783a7814ecff42048f1111af684455245
Lawson <abraham.lawson.dev@gmail.com>
2021-04-10 14:24:21 +0700

    learn pre-fetching

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { getSortedPostsData } from '../lib/post'
+
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
 
 const Profile = () => (
   <Image
@@ -12,7 +22,7 @@ const Profile = () => (
   />
 )
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -21,45 +31,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p>Link <Link href='posts/first-post'><a>First Post</a></Link></p>
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <section >
+        <h2 >Blog</h2>
+        <ul >
+          {allPostsData.map(({ id, date, title }) => (
+            <li  key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
       </main>
 
       <footer className={styles.footer}>
